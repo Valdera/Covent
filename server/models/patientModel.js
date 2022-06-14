1e5c875248f81d6dcc8bd78fe30762e0be7c6091
josh.frederich <51415146+Joshfrederich@users.noreply.github.com>
2022-06-14 23:14:04 +0700

    Service Table, Admin Table, Booking Table

--- server/models/patientModel.js
+++ server/models/patientModel.js
@@ -24,3 +24,113 @@ const patientSchema = new mongoose.Schema({
     },
   ],
 });
+
+const bookingSchema = new mongoose.Schema({
+  patientId:
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Patient"
+  },
+  status: {
+    type: String,
+    required: [true, "Please enter the status"],
+    enum: {
+      values: ['CREATED', 'ACCEPTED', 'DONE', 'CANCELLED'],
+      message: "{VALUE} is not supported",
+    }
+  },
+  service: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Service',
+    required: [true, 'Review must belong to a user']
+  },
+  scheduleId:
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Schedule",
+    },
+  acceptedBy:
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Admin",
+      },
+  createdAt: {
+    type: Date,
+    required: [true, "Please enter the date"],
+  },
+  updatedAt: {
+    type: Date,
+    required: [true, "Please enter the date"],
+  },
+  deletedAt: {
+    type: Date,
+    required: [true, "Please enter the date"],
+  },
+
+});
+
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+  },
+  role: {
+    type: String,
+    required: [true, "Please enter the role"],
+    enum: {
+      values: ['DOCTOR', 'STAFF'],
+      message: "{VALUE} is not supported",
+    }
+  },
+  createdAt: {
+    type: Date,
+    required: [true, "Please enter the date"],
+  },
+  updatedAt: {
+    type: Date,
+    required: [true, "Please enter the date"],
+  },
+  deletedAt: {
+    type: Date,
+    required: [true, "Please enter the date"],
+  }
+});
+
+const serviceSchema = new mongoose.Schema({
+  doctor: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Admin"
+  },
+  schedule: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: "Schedule",
+    },],
+  createdAt: {
+    type: Date,
+    required: [true, "Please enter the date"],
+  },
+  createdBy:
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "Admin",
+      },
+  updatedAt: {
+    type: Date,
+    required: [true, "Please enter the date"],
+  },
+  deletedAt: {
+    type: Date,
+    required: [true, "Please enter the date"],
+  }
+});
+
+
+bookingSchema.pre(/^find/, function(next) {
+
+  this.populate({
+    path: 'user',
+    select: 'name photo'
+  });
+  next();
+});
