c86d69ac04fc6e5f5b5e6b8cd11f2acba9267ae4
Valdera <f.valdera@yahoo.co.id>
2022-06-16 19:22:08 +0700

    valdera | add contoller and repository example

--- server/server.js
+++ server/server.js
@@ -4,10 +4,6 @@ const mongoose = require("mongoose");
 // File import
 const config = require("./config");
 const app = require("./app");
-const patientModel = require("./models/patientModel");
-const adminModel = require("./models/adminModel");
-const bookingModel = require("./models/bookingModel");
-const serviceModel = require("./models/serviceModel");
 
 // Connect to mongoDB database
 mongoose
