109b6bf385eb408f64a6775a56be939bd06be5c3
riefkyarif <riefkyar.ed@protonmail.com>
2022-06-18 15:24:32 +0700

    riefky | schedule edit

--- server/models/scheduleModel.js
+++ server/models/scheduleModel.js
@@ -1,1 +1,13 @@
-const a
+const mongoose = require("mongoose");
+
+const patientSchema = new mongoose.Schema({
+  available: {
+    type: Number,
+    required: [true, "Please enter your name"],
+  },
+
+});
+
+const Patient = mongoose.model("Patient", patientSchema);
+
+module.exports = Patient;
